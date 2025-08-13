e20adcee183d3586f2e2c36448b60a493b410145
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2025-08-12 20:39:21 -0400

    Made Footer For The Website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-        
+        <Footer />
       </Router>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import BannerImage from '../assets/theWall.jpeg';
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>
-      Hello world
+    <div className="home">
+      <div 
+        className="headerContainer" 
+        style={{ backgroundImage: `url(${BannerImage})`}}
+      >
+        <h1> Bluvera Tracker </h1>
+        <p> Simple. Neat. Always on track.</p>
+        <Link to="/Create">
+          <button> Create</button>
+        </Link>
+        <Link to="/View">
+          <button> View</button>
+        </Link>
+        <Link to="/Update">
+          <button> Update</button>
+        </Link>
+        <Link to="/Download">
+          <button> Download</button>
+        </Link>
+      </div>
     </div>
   )
 }
